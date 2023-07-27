a6620a3fba1835cf657f2dac3ccf285f27b37406
rudranshrocks7 <43137840+rudranshrocks7@users.noreply.github.com>
2023-07-27 11:46:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php
+echo "<h1>Page Heading</h1>";
+print "<p style='font-weight:bold; color:blue;'>This is a paragraph text that is styled with CSS.</p>";
+?>
  </body>
 </html>
